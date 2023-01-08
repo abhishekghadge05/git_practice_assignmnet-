efad4590ca8046964d66c702f73f857109b0a604
abhishekghadge05 <121336464+abhishekghadge05@users.noreply.github.com>
2023-01-08 19:08:06 +0530

    prime number problem is done

---
+++ prime.js
@@ -0,0 +1,13 @@
+let number=3;
+let factor=0;
+for(i=1;i<=3;i++){
+if(i%3==0){     //correct(3%i==0)
+factor++
+}
+}
+if(factor==2){
+console.log("prime number")
+}
+else{
+console.log("not a prime number")
+}
